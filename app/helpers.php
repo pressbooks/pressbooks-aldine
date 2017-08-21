9ac091cd0f428e24f0f5fe34eaf5ffbddec64c5a
Ned Zimmerman <ned@bight.ca>
2017-08-21 13:14:43 -0300

    Update to Sage 9.0.0-RC4.

--- app/helpers.php
+++ app/helpers.php
@@ -52,6 +52,10 @@ function config($key = null, $default = null)
  */
 function template($file, $data = [])
 {
+    if (remove_action('wp_head', 'wp_enqueue_scripts', 1)) {
+        wp_enqueue_scripts();
+    }
+
     return sage('blade')->render($file, $data);
 }
 
@@ -81,12 +85,25 @@ function asset_path($asset)
  */
 function filter_templates($templates)
 {
+    $paths = apply_filters('sage/filter_templates/paths', [
+        'views',
+        'resources/views'
+    ]);
+    $paths_pattern = "#^(" . implode('|', $paths) . ")/#";
+
     return collect($templates)
-        ->map(function ($template) {
-            return preg_replace('#\.(blade\.)?php$#', '', ltrim($template));
+        ->map(function ($template) use ($paths_pattern) {
+            /** Remove .blade.php/.blade/.php from template names */
+            $template = preg_replace('#\.(blade\.?)?(php)?$#', '', ltrim($template));
+
+            /** Remove partial $paths from the beginning of template names */
+            if (strpos($template, '/')) {
+                $template = preg_replace($paths_pattern, '', $template);
+            }
+
+            return $template;
         })
-        ->flatMap(function ($template) {
-            $paths = apply_filters('sage/filter_templates/paths', ['views', 'resources/views']);
+        ->flatMap(function ($template) use ($paths) {
             return collect($paths)
                 ->flatMap(function ($path) use ($template) {
                     return [
@@ -120,28 +137,4 @@ function display_sidebar()
     static $display;
     isset($display) || $display = apply_filters('sage/display_sidebar', false);
     return $display;
-}
-
-/**
- * Page titles
- * @return string
- */
-function title()
-{
-    if (is_home()) {
-        if ($home = get_option('page_for_posts', true)) {
-            return get_the_title($home);
-        }
-        return __('Latest Posts', 'pressbooks-aldine');
-    }
-    if (is_archive()) {
-        return get_the_archive_title();
-    }
-    if (is_search()) {
-        return sprintf(__('Search Results for %s', 'pressbooks-aldine'), get_search_query());
-    }
-    if (is_404()) {
-        return __('Not Found', 'pressbooks-aldine');
-    }
-    return get_the_title();
 }
--- resources/controllers/App.php
+++ resources/controllers/App.php
@@ -10,4 +10,24 @@ class App extends Controller
     {
         return get_bloginfo('name');
     }
+
+    public static function title()
+    {
+        if (is_home()) {
+            if ($home = get_option('page_for_posts', true)) {
+                return get_the_title($home);
+            }
+            return __('Latest Posts', 'pressbooks-aldine');
+        }
+        if (is_archive()) {
+            return get_the_archive_title();
+        }
+        if (is_search()) {
+            return sprintf(__('Search Results for %s', 'pressbooks-aldine'), get_search_query());
+        }
+        if (is_404()) {
+            return __('Not Found', 'pressbooks-aldine');
+        }
+        return get_the_title();
+    }
 }
