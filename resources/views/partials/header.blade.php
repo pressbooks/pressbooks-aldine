9b0849f99b5142e466b75c67b02e59456a7c5f83
Ned Zimmerman <ned@bight.ca>
2017-08-22 14:30:11 -0300

    Navigation bar.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,15 @@
 <header class="banner">
   <div class="container">
     <a class="toggle-menu" href="#primary-navigation">Toggle menu<span class="icon"></span></a>
+    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav id="primary-navigation" class="primary-navigation">
-        <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-        <a href="{{ home_url('/catalog') }}">Catalog</a>
-        <a href="#contact">Contact</a>
-        <a href="{{ wp_login_url() }}">{{ __('Sign in') }}</a>
-        <a href="{{ network_home_url('/wp-signup.php') }}">{{ __('Sign up') }}</a>
+        <a class="catalog" href="{{ home_url('/catalog') }}">Catalog</a>
+        <a class="contact" href="#contact">Contact</a>
+        @if(!is_user_logged_in())
+          <a class="signin" href="{{ wp_login_url() }}">{{ __('Sign in', 'pressbooks-aldine') }}</a>
+          <span class="sep">/</span>
+        @endif
+        <a class="signup" href="{{ network_home_url('/wp-signup.php') }}">{{ __('Sign up', 'pressbooks-aldine') }}</a>
     </nav>
     <div class="branding">
       <h1><a href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a></h1>
