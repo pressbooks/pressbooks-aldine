264635741f45e81dbdd736fcf57d44b1df778ede
Ned Zimmerman <ned@bight.ca>
2017-08-26 10:52:31 -0300

    Restore index.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,37 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-  @if($block_count === 0)
-    <div class="block block-1">
-      <div class="inside">
-        <h3>{{ __('About Pressbooks', 'aldine')}}</h3>
-        <p>{{ __('Pressbooks is easy-to-use book writing software that lets you create a book in all the formats you need to publish.', 'aldine')}}</p>
-      </div>
+  @include('partials.page-header')
+
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
     </div>
-  @elseif($block_count < 5)
-    @for($i = 0; $i < $block_count; $i++)
-      <div class="block block-{{ $i + 1 }}@if($latest_books_block === $home_blocks[$i]) latest-books @endif">
-        <div class="inside">
-          @php(dynamic_sidebar($home_blocks[$i]))
-        </div>
-      </div>
-    @endfor
-  @elseif($block_count === 5)
-    <div class="one-two">
-      @for($i = 0; $i < 2; $i++)
-        <div class="block block-{{ $i + 1 }}@if($latest_books_block === $home_blocks[$i]) latest-books @endif">
-          <div class="inside">
-            @php(dynamic_sidebar($home_blocks[$i]))
-          </div>
-        </div>
-      @endfor
-    </div>
-    @for($i = 2; $i < $block_count; $i++)
-      <div class="block block-{{ $i + 1 }}@if($latest_books_block === $home_blocks[$i]) latest-books @endif">
-        <div class="inside">
-          @php(dynamic_sidebar($home_blocks[$i]))
-        </div>
-      </div>
-    @endfor
+    {!! get_search_form(false) !!}
   @endif
+
+  @while (have_posts()) @php(the_post())
+    @include('partials.content-'.get_post_type())
+  @endwhile
+
+  {!! get_the_posts_navigation() !!}
 @endsection
