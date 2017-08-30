389ad19c965d9ae0e5a30700f2141de0fbf1835f
Ned Zimmerman <ned@bight.ca>
2017-08-30 10:56:38 -0300

    Clean up dashboard.

--- app/intervention.php
+++ app/intervention.php
@@ -8,6 +8,7 @@ if (function_exists('\Sober\Intervention\intervention')) {
     intervention('remove-customizer-items', 'static-front-page', 'all');
     intervention('remove-emoji');
     intervention('remove-howdy', __('Hello,', 'aldine'));
+    intervention('remove-dashboard-items', ['activity', 'quick-draft']);
     intervention('remove-menu-items', [
         'posts',
         'tools',
