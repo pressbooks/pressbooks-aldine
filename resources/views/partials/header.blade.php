4425526c59ad233e994093c775436c33554cf5b7
Ned Zimmerman <ned@bight.ca>
2017-09-12 14:49:27 -0300

    Update header.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,12 @@
-<section class="banner bg-bottom cover" style="background-image: url(@if(is_front_page())
+<section class="banner" style="background-image: url(@if(is_front_page())
   @php(header_image())
 @else
   @asset('images/catalog-header.jpg')
 @endif);">
-  <div class="container flex flex-column justify-between items-center w-100 mw-100">
-    <a class="toggle-menu db dn-l absolute" href="#primary-navigation">{{ __('Toggle menu', 'aldine') }}<span class="icon db absolute"></span></a>
-    <a class="brand db self-start self-center-l" href="{{ home_url('/') }}"><span class="clip">{{ get_bloginfo('name', 'display') }}</span>{!! $site_logo !!}</a>
-    <nav id="primary-navigation" class="primary-navigation absolute flex flex-column justify-center items-start top-0 w-100 bg-transparent bg-animate flex-row-l justify-start-l items-center-l">
+  <div class="container">
+    <a class="toggle-menu" href="#primary-navigation">{{ __('Toggle menu', 'aldine') }}<span class="icon"></span></a>
+    <a class="brand" href="{{ home_url('/') }}"><span class="clip">{{ get_bloginfo('name', 'display') }}</span>{!! $site_logo !!}</a>
+    <nav id="primary-navigation" class="primary-navigation">
         <a href="{{ home_url('/catalog') }}">Catalog</a>
         <a href="#contact">Contact</a>
         @if(!is_user_logged_in())
@@ -19,7 +19,7 @@
           <a href="{{ wp_logout_url() }}">{{ __('Sign out', 'aldine') }}</a>
         @endif
     </nav>
-    <header class="branding flex flex-column justify-start items-center tc">
+    <header class="branding">
       <h1><a href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a></h1>
       <p>{{ get_bloginfo('description', 'display') }}</p>
     </header>
