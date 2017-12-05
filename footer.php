3ac6e1cd21d93a54acb27147f4ce6821dbd7b36a
Ned Zimmerman <ned@bight.ca>
2017-12-05 16:05:58 -0400

    Add footer and header.

---
+++ footer.php
@@ -0,0 +1,6 @@
+        </div>
+        <?php do_action('get_footer');
+        echo \Aldine\template('partials/footer');
+        wp_footer(); ?>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <?= \Aldine\template('partials/head'); ?>
+    <body <?php body_class(); ?>>
+        <?php do_action('get_header'); ?>
+        <?= \Aldine\template('partials/header'); ?>
+        <div class="wrap container" role="document">
