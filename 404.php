ff635660f4ed6ef98535a45a48265be08a395f9f
Dac Chartrand <dac.chartrand@gmail.com>
2018-01-29 13:25:52 -0500

    Widgets not supported @see pressbooks-aldine/inc/intervention.php

--- 404.php
+++ 404.php
@@ -18,12 +18,10 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pressbooks-aldine' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'pressbooks-aldine' ); ?></p>
 
 					<?php
 						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
 					<div class="widget widget_categories">
@@ -40,15 +38,6 @@ get_header(); ?>
 						?>
 						</ul>
 					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'pressbooks-aldine' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
