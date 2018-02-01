1793386ff200a8bdc92d2bae27527f4a59ffbbc3
Ned Zimmerman <ned@bight.ca>
2018-01-31 22:05:21 -0400

    Fixes for #71 (#73)
    * Redirect empty catalog to 404, hide empty landing page catalog, hide content editor for catalog page.
    
    * PHPCS fix.
    
    * Use ??.

--- partials/content-front-page.php
+++ partials/content-front-page.php
@@ -37,7 +37,7 @@ if ( get_option( 'pb_front_page_catalog' ) ) {
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
 
-<?php if ( get_option( 'pb_front_page_catalog' ) ) : ?>
+<?php if ( get_option( 'pb_front_page_catalog' ) && ! empty( $catalog_data['books'] ) ) : ?>
 <div id="latest-books" class="latest-books">
 	<h2 id="latest-books-title"><?php echo $latest_books_title; ?></h2>
 	<div class="slider" role="region" aria-labelledby="latest-books-title" data-total-pages="<?php echo $catalog_data['pages']; ?>" <?php if ( $next_page <= $catalog_data['pages'] ) : ?>data-next-page="<?php echo $next_page; ?>"<?php endif; ?>>
--- partials/content-page-catalog.php
+++ partials/content-page-catalog.php
@@ -9,26 +9,6 @@
 
 ?>
 
-<?php
-
-use function Aldine\Helpers\get_catalog_data;
-use function Aldine\Helpers\get_catalog_licenses;
-use function Aldine\Helpers\get_available_subjects;
-use function Aldine\Helpers\get_available_licenses;
-
-$current_page = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-$orderby = ( get_query_var( 'orderby' ) ) ? get_query_var( 'orderby' ) : 'title';
-$subject = ( get_query_var( 'subject' ) ) ? get_query_var( 'subject' ) : '';
-$license = ( get_query_var( 'license' ) ) ? get_query_var( 'license' ) : '';
-$catalog_data = get_catalog_data( $current_page, 9, $orderby, $license, $subject );
-$previous_page = ( $current_page > 1 ) ? $current_page - 1 : 0;
-$next_page = $current_page + 1;
-$licenses = get_catalog_licenses();
-$available_licenses = get_available_licenses( $catalog_data );
-$subjects = ( defined( 'PB_PLUGIN_VERSION' ) ) ? \Pressbooks\Metadata\get_thema_subjects() : [];
-$available_subjects = get_available_subjects( $catalog_data );
-?>
-
 <?php get_template_part( 'partials/page', 'header' ); ?>
 <section class="network-catalog">
 	<form role="form" class="filter-sort" method="get">
