6b8008909d2e05cb5f159904271004ea472167b7
Ned Zimmerman <ned@bight.ca>
2018-04-27 08:55:46 -0300

    Fix lint errors.

--- assets/scripts/call-to-action.js
+++ assets/scripts/call-to-action.js
@@ -2,21 +2,21 @@
 	tinymce.create( 'tinymce.plugins.aldine_call_to_action', {
 		init: function ( editor, url ) {
 			editor.addButton( 'aldine_call_to_action', {
-				title:   aldine.call_to_action.title,
-				icon:    'icon dashicons-flag',
+				title: aldine.call_to_action.title,
+				icon: 'icon dashicons-flag',
 				onclick: function () {
 					editor.windowManager.open( {
 						title: aldine.call_to_action.title,
-						body:  [
+						body: [
 							{
-								type:  'textbox',
-								name:  'text',
+								type: 'textbox',
+								name: 'text',
 								label: aldine.call_to_action.text,
 								value: aldine.call_to_action.title,
 							},
 							{
-								type:  'textbox',
-								name:  'link',
+								type: 'textbox',
+								name: 'link',
 								label: aldine.call_to_action.link,
 								value: '#',
 							},
--- assets/scripts/catalog-admin.js
+++ assets/scripts/catalog-admin.js
@@ -2,40 +2,85 @@
 ( function ( $ ) {
 	$( document ).ready( function () {
 		$( '.wrap' ).on( 'click', '.notice-dismiss', function () {
-			$( this ).parent( '#message' ).fadeOut( 500, function () {
-				$( this ).remove();
-			} );
+			$( this )
+				.parent( '#message' )
+				.fadeOut( 500, function () {
+					$( this ).remove();
+				} );
 		} );
 		$( 'input.in-catalog' ).on( 'change', function () {
-			let book_id = $( this ).parent( 'td' ).siblings( 'th' ).children( 'input' ).val();
+			let book_id = $( this )
+				.parent( 'td' )
+				.siblings( 'th' )
+				.children( 'input' )
+				.val();
 			let in_catalog = $( this ).prop( 'checked' );
 			$.ajax( {
-				url:  ajaxurl,
+				url: ajaxurl,
 				type: 'POST',
 				data: {
-					action:      'pressbooks_aldine_update_catalog',
-					book_id:     book_id,
-					in_catalog:  in_catalog,
+					action: 'pressbooks_aldine_update_catalog',
+					book_id: book_id,
+					in_catalog: in_catalog,
 					_ajax_nonce: PB_Aldine_Admin.aldineAdminNonce,
 				},
-				success: function (){
+				success: function () {
 					if ( $( '#message' ).length < 1 ) {
-						$( '<div id="message" class="updated notice is-dismissible">' ).html( '<p><strong>'+PB_Aldine_Admin.catalog_updated+'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">'+PB_Aldine_Admin.dismiss_notice+'</span></button>' ).hide().insertAfter( '.wrap h1' ).fadeIn( 500 );
+						$( '<div id="message" class="updated notice is-dismissible">' )
+							.html(
+								'<p><strong>' +
+									PB_Aldine_Admin.catalog_updated +
+									'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +
+									PB_Aldine_Admin.dismiss_notice +
+									'</span></button>'
+							)
+							.hide()
+							.insertAfter( '.wrap h1' )
+							.fadeIn( 500 );
 					} else {
 						$( '#message' ).fadeOut( 500, function () {
 							$( this ).remove();
-							$( '<div id="message" class="updated notice is-dismissible">' ).html( '<p><strong>'+PB_Aldine_Admin.catalog_updated+'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">'+PB_Aldine_Admin.dismiss_notice+'</span></button>' ).hide().insertAfter( '.wrap h1' ).fadeIn( 500 );
+							$( '<div id="message" class="updated notice is-dismissible">' )
+								.html(
+									'<p><strong>' +
+										PB_Aldine_Admin.catalog_updated +
+										'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +
+										PB_Aldine_Admin.dismiss_notice +
+										'</span></button>'
+								)
+								.hide()
+								.insertAfter( '.wrap h1' )
+								.fadeIn( 500 );
 						} );
 					}
-
 				},
 				error: function ( jqXHR, textStatus, errorThrown ) {
 					if ( $( '#message' ).length < 1 ) {
-						$( '<div id="message" class="error notice is-dismissible">' ).html( '<p><strong>'+PB_Aldine_Admin.catalog_not_updated+'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">'+PB_Aldine_Admin.dismiss_notice+'</span></button>' ).hide().insertAfter( '.wrap h1' ).fadeIn( 500 );
+						$( '<div id="message" class="error notice is-dismissible">' )
+							.html(
+								'<p><strong>' +
+									PB_Aldine_Admin.catalog_not_updated +
+									'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +
+									PB_Aldine_Admin.dismiss_notice +
+									'</span></button>'
+							)
+							.hide()
+							.insertAfter( '.wrap h1' )
+							.fadeIn( 500 );
 					} else {
 						$( '#message' ).fadeOut( 500, function () {
 							$( this ).remove();
-							$( '<div id="message" class="error notice is-dismissible">' ).html( '<p><strong>'+PB_Aldine_Admin.catalog_not_updated+'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">'+PB_Aldine_Admin.dismiss_notice+'</span></button>' ).hide().insertAfter( '.wrap h1' ).fadeIn( 500 );
+							$( '<div id="message" class="error notice is-dismissible">' )
+								.html(
+									'<p><strong>' +
+										PB_Aldine_Admin.catalog_not_updated +
+										'</strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +
+										PB_Aldine_Admin.dismiss_notice +
+										'</span></button>'
+								)
+								.hide()
+								.insertAfter( '.wrap h1' )
+								.fadeIn( 500 );
 						} );
 					}
 				},
