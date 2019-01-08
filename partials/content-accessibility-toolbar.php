d29517768b4051fd691d87c17f7b15cf42963e92
Ned Zimmerman <ned@bight.ca>
2019-01-08 09:50:57 -0400

    Update Aetna, simplify font size button

--- partials/content-accessibility-toolbar.php
+++ partials/content-accessibility-toolbar.php
@@ -1,13 +1,7 @@
 <!-- a11y toolbar -->
 <div class="a11y-toolbar">
-	<div class="wrap">
-	  <ul>
-			<li>
-				<a href="#" role="button" class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" title="<?php _e( 'Increase Font Size', 'pressbooks-aldine' ); ?>">
-					<?php _e( 'Increase Font Size', 'pressbooks-aldine' ); ?>
-				</a>
-			</li>
-		</ul>
-	</div>
+	<button class="a11y-fontsize" aria-pressed="false">
+		<?php _e( 'Increase Font Size', 'pressbooks-aldine' ); ?>
+	</button>
 </div>
 <!-- // a11y toolbar -->
