388e5ca6244044655436cd5455379341dff3de1a
Ned Zimmerman <ned@bight.dev>
2020-12-04 16:14:45 -0400

    ci: fix unit test

--- tests/test-filters.php
+++ tests/test-filters.php
@@ -5,12 +5,14 @@
  * @package Pressbooks_Aldine
  */
 
+use function \Aldine\Filters\register_query_vars;
+
 /**
  * Filters test case.
  */
 class FiltersTest extends WP_UnitTestCase {
 	public function test_register_query_vars() {
-		$vars = \Aldine\Helpers\register_query_vars([]);
+		$vars = register_query_vars([]);
 		$this->assertContains('license', $vars);
 		$this->assertContains('subject', $vars);
 	}
