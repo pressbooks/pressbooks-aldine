475ced255fdfcccf2a119746e4c2cacd62bcbed2
arzola <oscararzola@gmail.com>
2021-07-22 15:20:21 -0500

    Use classic widgets in WP 5.8

--- inc/actions/namespace.php
+++ inc/actions/namespace.php
@@ -95,6 +95,13 @@ function setup() {
 
 	// Add shortcode buttons.
 	add_action( 'init', __NAMESPACE__ . '\register_shortcode_buttons' );
+
+	remove_theme_support( 'widgets-block-editor' );
+
+	// Disables the block editor from managing widgets in the Gutenberg plugin.
+	add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+	// Disables the block editor from managing widgets.
+	add_filter( 'use_widgets_block_editor', '__return_false' );
 }
 
 /**
