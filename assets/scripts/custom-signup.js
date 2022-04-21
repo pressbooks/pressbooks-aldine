67dd74adb9ed57b21bc275f18f136b55f1a6debb
SteelWagstaff <steel@pressbooks.com>
2022-04-21 16:53:11 -0700

    lint

--- assets/scripts/custom-signup.js
+++ assets/scripts/custom-signup.js
@@ -1,13 +1,13 @@
-import Typed from "typed.js";
+import Typed from 'typed.js';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener( 'DOMContentLoaded', function () {
 	let options = {
-		strings: ['open textbook', 'scholarly monograph', 'world-changing manifesto', 'graduate thesis', 'reference guide', 'essay collection', 'student portfolio', 'novel', 'position paper', 'handbook', 'magnum opus', 'research report', 'daybook', 'collected works'],
+		strings: [ 'open textbook', 'scholarly monograph', 'world-changing manifesto', 'graduate thesis', 'reference guide', 'essay collection', 'student portfolio', 'novel', 'position paper', 'handbook', 'magnum opus', 'research report', 'daybook', 'collected works' ],
 		typeSpeed: 80,
 		backSpeed: 40,
 		backDelay: 400,
 		loop: true,
 	};
 
-	new Typed('#typed', options);
-});
+	new Typed( '#typed', options );
+} );
