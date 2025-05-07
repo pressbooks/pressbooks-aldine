77a6aaeec962b97434228e15a3593860fc3cb879
Ricardo Aragon <richard015ar@gmail.com>
2025-05-07 09:52:45 -0300

    feat: add link to latest book in the home page

--- partials/featured-book.php
+++ partials/featured-book.php
@@ -8,7 +8,10 @@
 use function \Aldine\Helpers\maybe_truncate_string;
 ?>
 <div class="featured_book">
-	<div class="featured_book__cover" style="background-image: url('<?php echo $book['metadata']['image']; ?>' );"></div>
+	<!-- accessible link -->
+	<a href="<?php echo $book['link']; ?>" aria-label="<?php echo esc_attr( $book['metadata']['name'] ); ?>">
+		<div class="featured_book__cover" style="background-image: url('<?php echo $book['metadata']['image']; ?>' );"></div>
+	</a>
 	<p class="featured_book__title">
 		<a href="<?php echo $book['link']; ?>"><?php echo maybe_truncate_string( $book['metadata']['name'] ); ?></a>
 	</p>
