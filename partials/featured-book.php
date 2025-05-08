88cc0c58092a52b7a9f234b3f85a92497eedd1b6
Felipe Dalcin <felipe@pressbooks.com>
2025-05-08 12:56:55 -0400

    fix: wrap entire book with an anchor tag

--- partials/featured-book.php
+++ partials/featured-book.php
@@ -10,8 +10,8 @@ use function \Aldine\Helpers\maybe_truncate_string;
 <div class="featured_book">
 	<a href="<?php echo $book['link']; ?>" aria-label="<?php echo esc_attr( $book['metadata']['name'] ); ?>">
 		<div class="featured_book__cover" style="background-image: url('<?php echo $book['metadata']['image']; ?>' );"></div>
+		<p class="featured_book__title">
+			<?php echo maybe_truncate_string( $book['metadata']['name'] ); ?>
+		</p>
 	</a>
-	<p class="featured_book__title">
-		<a href="<?php echo $book['link']; ?>"><?php echo maybe_truncate_string( $book['metadata']['name'] ); ?></a>
-	</p>
 </div>
